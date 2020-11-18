7a6999e7d3aafb45ffda3955a9192e92f6cdf470
Tangi Mendes <tangimds@gmail.com>
2020-11-18 19:22:43 +0100

    update housekeeping

--- housekeeping.sh
+++ housekeeping.sh
@@ -1,33 +1,41 @@
+#!/bin/sh
+
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+BLUE=$(tput setaf 4)
+BOLD=$(tput bold)
+NC=$(tput sgr0)
+
 # Update brew 
 clear
-echo "$fg_bold[blue] Updating brew"
+echo "${BOLD}${GREEN}Updating brew${NC}"
 brew update
 sleep 5 
 
 
 # Upgrade Brew Cask
 clear
-echo "$fg_bold[blue] Updating brew cask"
+echo "${BOLD}${GREEN}Updating brew cask${NC}"
 brew upgrade --cask
 sleep 5 
 
 
 # Check the local Homebrew installation for potential problems
 clear
-echo "$fg_bold[blue] Running brew docktor"
+echo "${BOLD}${GREEN}Running brew docktor${NC}"
 brew doctor --verbose
 sleep 5
 
 #Remove stale lock files and outdated downloads for all formulae and casks, andremove old versions of installed formulae
 clear
-echo "$fg_bold[blue] running brew cleanup"
+echo "${BOLD}${GREEN}Running brew cleanup${NC}"
 brew cleanup -v
 sleep 5 
 
 
 # Brew show all outdated brew apps 
 clear
-echo "$fg_bold[blue] running brew outdated"
+echo "${BOLD}${GREEN}running brew outdated${NC}"
 brew outdated 
 sleep 5
 
