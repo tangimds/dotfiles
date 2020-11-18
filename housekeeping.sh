3a5f911b6cd49cbabd416db25b11a2a7a4eb0651
Tangi Mendes <tangimds@gmail.com>
2020-11-18 19:04:02 +0100

    Update housekeeping.sh

--- housekeeping.sh
+++ housekeeping.sh
@@ -1,36 +1,36 @@
 # Update brew 
-echo "Updating brew"
+echo "$fg_bold[blue] Updating brew"
 brew update
 sleep 5 
 
 
 # Upgrade Brew Cask 
-echo "Updating brew cask"
+echo "$fg_bold[blue] Updating brew cask"
 brew upgrade --cask
 sleep 5 
 
 
 # Check the local Homebrew installation for potential problems
-echo "Running brew docktor"
+echo "$fg_bold[blue] Running brew docktor"
 brew doctor --verbose
 sleep 5
 
 #Remove stale lock files and outdated downloads for all formulae and casks, andremove old versions of installed formulae
-echo "running brew cleanup"
+echo "$fg_bold[blue] running brew cleanup"
 brew cleanup -v
 sleep 5 
 
 
 # Brew show all outdated brew apps 
-echo "running brew outdated"
+echo "$fg_bold[blue] running brew outdated"
 brew outdated 
 sleep 5
 
 # Shows all the outdated cask apps 
-echo "Printing all apps need to be updated"
-brew cu -f 
-sleep 5 
+#echo "Printing all apps need to be updated"
+#brew cu -f 
+#sleep 5 
 
 # Force Updagrade all the outdated Brew cask apps 
-echo "Do you want to force upgrade brew apps ? Y/N "
-brew cu -f -a
+#echo "Do you want to force upgrade brew apps ? Y/N "
+#brew cu -f -a
